947294eaf995e8d22e73172027d5a00963aa3961
Ethan Yun <ethanyun@ohs.stanford.edu>
2022-10-14 15:58:27 -0700

    Actually mobilenet later, now now

--- src/model.py
+++ src/model.py
@@ -2,30 +2,29 @@ import tensorflow as tf
 
 # Darknet cells
 class DarknetBlock(tf.keras.Model):
-	def __init__(self, stride, filters):
+	def __init__(self, filters):
 		super().__init__()
 
-		# Layer for depthwise convolution
-		self.depth_conv2d = tf.keras.layers.DepthwiseConv2D(3, stride)
+		# Layer for conv 1x1
+		self.one_conv2d = tf.keras.layers.Conv2D(filters, 1, 1, padding="same")
 		self.batchnorm1 = tf.keras.layers.BatchNormalization()
-		# Layer for 1x1 convolution
-		self.one_conv2d = tf.keras.layers.Conv2D(filters, 1)
+		# Layer for conv 3x3
+		self.three_conv2d = tf.keras.layers.Conv2D(filters*2, 3, 1, padding="same")
 		self.batchnorm2 = tf.keras.layers.BatchNormalization()
-		# Layer for depthwise concatenation
-		self.concat = tf.keras.layers.Concatenate()
 
 	def call(self, input_tensor, training=False):
 		# Calculate the stuff
-		# The depth stuff
-		x = self.depth_conv2d(input_tensor)
+		# The conv stuff 1x1
+		x = self.one_conv2d(nput_tensor)
 		x = self.batchnorm1(x, training=training)
 		x = tf.nn.leaky_relu(x, alpha=0.1)
 
-		# The conv stuff 1x1
-		x = self.one_conv2d(x)
+		# The conv 3x3 stuff
+		x = self.three_conv2d(x)
 		x = self.batchnorm2(x, training=training)
+		x = tf.nn.leaky_relu(x, alpha=0.1)
 
 		# The stuff for residual connections
 		x += input_tensor
 
-		return tf.nn.leaky_relu(x, alpha=0.1)
+		return x
